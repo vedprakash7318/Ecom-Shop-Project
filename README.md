78e7e9c08688d21f932c23cb09c11c6852458c31
DigiCoders Technologies <digicodersdevelopment@gmail.com>
2026-01-12 16:06:50 +0530

    Added but now feature and cupan code

--- README.md
+++ README.md
@@ -34,3 +34,32 @@ show item with details :-  two COD UPI(GPay,Phonepay)  sweetAlert
 Seraching & filter :- 
 
 
+
+Payment Gateway  opt 
+
+Razor Pay 
+
+PhonePay
+
+Paypal
+
+
+
+
+
+Public Key   (Safe)   frontend plce 
+ 
+Secret Key  (locker)   backned 
+
+
+razorpay popup open 
+
+Payment UI 
+
+
+
+
+
+
+
+
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useCart } from '../Context/CartContext'
-
+import Swal from 'sweetalert2'
 const Cart = () => {
 const {cart,IncraeseQty,DecraeseQty,removeItem} = useCart()
+const discount = 5
+const [showForm,setShowForm] =useState(false)
+const [orderData,setOrderData] =useState({
+    name :"",
+    mobile:"",
+    address:""
+})
+const [cupanCode,setCupanCode] =useState("")
 
+const matchCupanCode = "VED123"
+
+const handleChange =(e)=>{
+    setOrderData({
+        ...orderData,[e.target.name]:e.target.value
+    })
+}
+
+const handleSubmit=(e)=>{
+    e.preventDefault()
+    if(!orderData.name || !orderData.address || !orderData.mobile){
+        Swal.fire({
+            title: 'Error!',
+            text: 'Please Fill all field',
+            icon: 'error',
+        })
+        return
+    }   
+    const order = {
+        customer :orderData,
+        product:cart,
+        totalAmount:total,
+        orderDate:new Date().toLocaleString()
+    }
+
+    localStorage.setItem("myOrder",JSON.stringify(order))
+    Swal.fire({
+            icon: 'success',
+            title: 'Order Confirmed',
+            text: 'Your Order has beed placed successfully',
+            confirmButtonColor:"#3085d6"
+        })
+    setShowForm(false)
+    setOrderData({
+        name:"",
+        mobile:"",
+        address:""
+    })
+
+}
+const [finalTotal,setfinalTotal] =useState(0)
+const handleApplyCupon =()=>{
+    if(cupanCode===matchCupanCode){
+         Swal.fire({
+            icon: 'success',
+            title: 'Discount Apply',
+            text: 'Your discount has beed apply successfully',
+            confirmButtonColor:"#3085d6"
+        })
+    const a = total-discount
+    setfinalTotal(a)
+
+    }
+    console.log(cupanCode)
+}
 const total = cart.reduce((sum,item)=>sum+item.price*item.qty,0)
-
+useEffect(()=>{
+    setfinalTotal(total)
+},[])
   return (
     <>
         <div style={{padding:"20px"}}>
@@ -33,13 +98,79 @@ const total = cart.reduce((sum,item)=>sum+item.price*item.qty,0)
             </div>
         ))}
 
+        <p><input type="text" onChange={(e)=>setCupanCode(e.target.value)} value={cupanCode} /> <button onClick={handleApplyCupon}>Apply</button></p>
 
-        <h2>Total Price : {total}rs</h2>
+        <h2>Total Price : {finalTotal}rs</h2>
 
+
+        <button onClick={()=>setShowForm(true)}>Buy Now</button>
         </div>
-    
+
+
+    {showForm && (
+        <div style={modalStyle}>
+            <form onSubmit={handleSubmit} style={formStyle}>
+                <h2>Order Details</h2>
+
+                <input 
+                type="text" 
+                name='name'
+                placeholder='Full name'
+                value={orderData.name}
+                onChange={handleChange}
+                style={inputStyle}
+                />
+                <input 
+                type="tel" 
+                name='mobile'
+                placeholder='Mobile Number'
+                value={orderData.mobile}
+                onChange={handleChange}
+                style={inputStyle}
+                />
+                <textarea
+                name="address"
+                placeholder='Delivery Address'
+                value={orderData.address}
+                onChange={handleChange}
+                style={inputStyle}
+                />
+
+                <div style={{display:"flex", gap:"10px"}}>
+                    <button type='submit'>Confirm Order</button>
+                    <button type='button' onClick={()=>setShowForm(false)}>Cancel</button>
+                </div>
+            </form>
+        </div>
+    )}
     </>
   )
 }
 
 export default Cart
+
+
+
+const modalStyle ={
+    position:"fixed",
+    top:0,
+    left:0,
+    width:"100%",
+    height:"100%",
+    background:"rgba(0,0,0,0.5)",
+    display:"flex",
+    justifyContent:"center",
+    alignItem:"center"
+
+}
+const formStyle ={
+    background:"#fff",
+    padding:"10px",
+    width:"500px"
+}
+
+const inputStyle = {
+    width:"90%",
+    padding:"9px",
+    marginBottom:"10px"
+}
